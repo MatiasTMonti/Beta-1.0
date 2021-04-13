d2ef3e0bc2ff1e442dcf0588f0181f75f4b5d7c7
Matias <matiasmonti2011@hotmail.com>
2021-04-13 18:02:59 -0300

    .

---
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    static int mainScene = 0;
+
+    [SerializeField] private AudioClip ClickSfx = null;
+
+    public void ShopFunction()
+    {
+        SceneManager.LoadScene(1);
+        AudioController.instance.PlaySound(ClickSfx);
+    }
+
+    public void PlayFunction()
+    {
+        SceneManager.LoadScene(2);
+        AudioController.instance.PlaySound(ClickSfx);
+    }
+
+    public static void LoadMainScene()
+    {
+        SceneManager.LoadScene(mainScene);
+    }
+
+    public void SetVolume(float vol)
+    {
+        AudioController.instance.SetVolume(vol);
+    }
+}
